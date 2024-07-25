cd57eac7f02f1008fa786381076da8ed8f8ac8f7
MCavieres <maca.c.91@gmail.com>
2024-07-25 14:46:27 -0400

    modularizacion

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const App = () => {
-    return (
-        <div>
-            <h1>Hola mundo</h1>
-        </div>
-    );
-};
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>

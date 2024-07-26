a1b743e5ac0e0e156fe432ac94f5ee53b96783bc
MCavieres <maca.c.91@gmail.com>
2024-07-26 09:37:35 -0400

    practica de proptypes

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -3,17 +3,31 @@
 //     title: "New World",
 // };
 
-const getResult = (a, b) => {
-    return a + b;
-};
+// const getResult = (a, b) => {
+//     return a + b;
+// };
 
-export const FirstApp = () => {
+import PropTypes from "prop-types";
+
+export const FirstApp = ({ title, subtitle }) => {
     return (
         <>
             <h1>Hola mundo</h1>
             {/* <code>{JSON.stringify(newMessage)}</code> */}
-            <h3>{getResult(4, 8)}</h3>
+            {/* <h3>{getResult(4, 8)}</h3> */}
+            <h4>{title}</h4>
+            <h4>{subtitle + 3}</h4>
             <p>Primer componente</p>
         </>
     );
 };
+
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.number.isRequired,
+};
+
+// FirstApp.defaultProps = {
+//     title: "No hay titulo",
+//     subtitle: "de nuevo no hay subtitulo",
+// };
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 // import App from "./App";
 
 import { FirstApp } from "./FirstApp";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         {/* <App /> */}
-        <FirstApp />
+        <FirstApp title="Este es el titulo" subtitle={432} />
     </React.StrictMode>
 );
